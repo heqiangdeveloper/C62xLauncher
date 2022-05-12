9624ba2a440811aee2bdf3426b9c822137aad838
yangdu <yang.d@haibingtech.com>
2022-05-12 11:32:26 +0800

    Init Commit.
    Change-Id: If67ae061d8aa109f781d333d098923e12a596e1a

---
+++ app/src/main/java/com/chinatsp/launcher/CarLauncher.java
@@ -0,0 +1,18 @@
+package com.chinatsp.launcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class CarLauncher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
