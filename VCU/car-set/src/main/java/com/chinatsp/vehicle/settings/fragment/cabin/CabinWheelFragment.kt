2b5d1bccc12b52303ddc21d71af059ca531c1953
wuyuanlin <wu.yl@haibingtech.com>
2022-06-06 13:40:51 +0800

    添加方向盘自动加热、方向盘自定义案件UI
    Change-Id: I8b6eb6a3cf301ee105cc4d7c4030060cbea1fcde

--- VCU/car-set/src/main/java/com/chinatsp/vehicle/settings/fragment/cabin/CabinWheelFragment.kt
+++ VCU/car-set/src/main/java/com/chinatsp/vehicle/settings/fragment/cabin/CabinWheelFragment.kt
@@ -2,8 +2,9 @@ package com.chinatsp.vehicle.settings.fragment.cabin
 
 import android.os.Bundle
 import com.chinatsp.vehicle.settings.R
-import com.chinatsp.vehicle.settings.databinding.CabinAcFragmentBinding
 import com.chinatsp.vehicle.settings.databinding.CabinWhellFragmentBinding
+import com.chinatsp.vehicle.settings.fragment.cabin.dialog.SteeringDialogFragment
+import com.chinatsp.vehicle.settings.fragment.cabin.dialog.SteeringHeatingDialogFragment
 import com.chinatsp.vehicle.settings.vm.CabinACViewModel
 import com.common.library.frame.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,12 +17,35 @@ import dagger.hilt.android.AndroidEntryPoint
  * @version: 1.0
  */
 @AndroidEntryPoint
-class CabinWheelFragment: BaseFragment<CabinACViewModel, CabinWhellFragmentBinding>() {
+class CabinWheelFragment : BaseFragment<CabinACViewModel, CabinWhellFragmentBinding>() {
+    var steeringDialogFragment: SteeringDialogFragment? = null
+    private var steeringHeatingDialogFragment: SteeringHeatingDialogFragment? = null
     override fun getLayoutId(): Int {
         return R.layout.cabin_whell_fragment
     }
 
     override fun initData(savedInstanceState: Bundle?) {
+        setCheckedChangeListener()
+    }
 
+    private fun setCheckedChangeListener() {
+        binding.cabinWhellPressKey.setOnClickListener {
+            steeringDialogFragment = SteeringDialogFragment()
+            activity?.let { it1 ->
+                steeringDialogFragment!!.show(
+                    it1.supportFragmentManager,
+                    "steeringDialog"
+                )
+            }
+        }
+        binding.cabinWhellAutomaticHeating.setOnClickListener {
+            steeringHeatingDialogFragment = SteeringHeatingDialogFragment()
+            activity?.let { it1 ->
+                steeringHeatingDialogFragment!!.show(
+                    it1.supportFragmentManager, "steeringHeatingDialog"
+                )
+            }
+
+        }
     }
 }
