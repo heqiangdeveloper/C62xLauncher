d106a873813bd0775ca20f0f48180ee953ce4e18
he.q@haibingtech.com <he.q@haibingtech.com>
2022-06-20 15:02:52 +0800

    添加应用安装卸载的监听
    Change-Id: Ifc05c9fd3f1a2f566c8d54fee6f718b4d4e59256

--- C62xLauncher/app/src/main/java/com/chinatsp/launcher/CarLauncher.java
+++ C62xLauncher/app/src/main/java/com/chinatsp/launcher/CarLauncher.java
@@ -5,23 +5,43 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 
 import com.chinatsp.apppanel.ApppanelActivity;
+import com.chinatsp.apppanel.receiver.AppInstallStatusReceiver;
 
 public class CarLauncher extends AppCompatActivity {
+    private IntentFilter intentFilter;
+    private AppInstallStatusReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //注册监听APP安装卸载广播
+        registerAppInstallBroadcast();
     }
 
     public void toApppanel(View view){
         startActivity(new Intent(CarLauncher.this, ApppanelActivity.class));
     }
 
+    private void registerAppInstallBroadcast(){
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_PACKAGE_ADDED);
+        intentFilter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        intentFilter.addDataScheme("package");
+        receiver = new AppInstallStatusReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
 }
---
+++ C62xLauncher/apppanel/src/main/java/com/chinatsp/apppanel/receiver/AppInstallStatusReceiver.java
@@ -0,0 +1,98 @@
+package com.chinatsp.apppanel.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+import com.anarchy.classifyview.event.AppInstallStatusEvent;
+import com.anarchy.classifyview.event.Event;
+import com.chinatsp.apppanel.AppConfigs.AppLists;
+import com.chinatsp.apppanel.adapter.MyAppInfoAdapter;
+import com.chinatsp.apppanel.bean.LocationBean;
+import com.chinatsp.apppanel.db.MyAppDB;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppInstallStatusReceiver extends BroadcastReceiver {
+    private static final String TAG = "AppInstallStatusReceiver";
+    private MyAppDB db;
+    private LocationBean locationBean;
+    private int maxParentIndex = -1;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED)){
+            Log.d(TAG,"onReceive ACTION_PACKAGE_ADDED");
+            //安装成功
+            String packageName = intent.getData().getSchemeSpecificPart();
+            if(!AppLists.isInBlackListApp(packageName)){
+                db = new MyAppDB(context);
+                if(db.isExistPackage(packageName) == 0){//如果数据库中没有记录
+                    locationBean = new LocationBean();
+                    maxParentIndex = db.getMaxParentIndex();
+                    A:for(ResolveInfo info : getApps(context)){
+                        if(packageName.equals(info.activityInfo.packageName)){
+                            locationBean.setParentIndex(++maxParentIndex);
+                            locationBean.setChildIndex(-1);
+                            locationBean.setPackageName(info.activityInfo.packageName);
+                            locationBean.setName((info.activityInfo.loadLabel(context.getPackageManager())).toString());
+                            Drawable drawable = info.activityInfo.loadIcon(context.getPackageManager());
+
+                            locationBean.setImgDrawable(drawable);
+                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                            Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+                            Canvas canvas = new Canvas(bitmap);
+                            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                            drawable.draw(canvas);
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                            locationBean.setImgByte(baos.toByteArray());
+
+                            locationBean.setTitle("");
+                            locationBean.setAddBtn(0);
+                            locationBean.setStatus(0);
+                            locationBean.setPriority(0);
+                            locationBean.setInstalled(1);
+                            locationBean.setCanuninstalled(AppLists.packageUninstallStatus(info.activityInfo.packageName));
+                            break A;
+                        }
+                    }
+
+                    //写入数据库中
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            db.insertLocation(locationBean);
+                            EventBus.getDefault().post(new AppInstallStatusEvent(1,packageName));
+                        }
+                    }).start();
+                }else {
+                    Log.d(TAG,"already installed： " + packageName);
+                }
+            }
+        }else if(intent.getAction().equals(Intent.ACTION_PACKAGE_REMOVED)){
+            //卸载成功
+            String packageName = intent.getData().getSchemeSpecificPart();
+            EventBus.getDefault().post(new AppInstallStatusEvent(0,packageName));
+        }
+    }
+
+    /**
+     * 获取系统中所有的APP
+     * @return
+     */
+    private List<ResolveInfo> getApps(Context context){
+        PackageManager packageManager = context.getPackageManager();
+        Intent i = new Intent(Intent.ACTION_MAIN,null);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        return packageManager.queryIntentActivities(i,0);
+    }
+}
