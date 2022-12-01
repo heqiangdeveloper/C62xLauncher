70387aae5173d316fceb2e028cfdde34cc5b6250
wuyuanlin <wu.yl@haibingtech.com>
2022-12-01 17:22:24 +0800

    修改车辆设置英文
    Change-Id: Ic6df20ee2a7144bb3e8d4b26d427b0529ba79cf7

--- VCU/car-set/src/main/java/com/chinatsp/vehicle/settings/fragment/doors/CarTrunkFragment.kt
+++ VCU/car-set/src/main/java/com/chinatsp/vehicle/settings/fragment/doors/CarTrunkFragment.kt
@@ -1,7 +1,6 @@
 package com.chinatsp.vehicle.settings.fragment.doors
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
