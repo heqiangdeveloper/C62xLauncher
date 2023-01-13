2cb7ae397e52d0e27d2e4045c3dd6716c567bd01
yangdu <yang.d@haibingtech.com>
2023-01-13 14:21:32 +0800

    <fix>[card-db]: 主页卡片数据库未处理版本升级的闪退
    原因: 此bug是为了修复另一个bug(83853)造成的.
         bug(83853) : 旧版本card-db中的主键为id(自增), 但由于room框架问题, 第一次初始化数据库和表之后, 无法正常update数据, 会造成车机重置数据之后, 无法保存卡片编辑结果.
         在修复bug(83853)中,  删除了字段id, 使用卡片类型type作为主键.
         但由于没有提交版本号变更(1->2), 且未做MIGRATION, 导致数据库读取数据错误, 造成闪退.
    对策: 提交db版本变更, 并做MIGRATION处理, 做数据迁移工作.
    
    关闭 禅道#83853
    
    Change-Id: Ic9e003a7a59a643f41aca3303b2a46050f757fc8

--- C62xLauncher/baseCard/src/main/java/card/db/CardDataBaseService.java
+++ C62xLauncher/baseCard/src/main/java/card/db/CardDataBaseService.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 
+import androidx.annotation.NonNull;
 import androidx.room.Room;
+import androidx.room.migration.AutoMigrationSpec;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -14,10 +18,12 @@ import card.service.R;
 import launcher.base.async.AsyncSchedule;
 import launcher.base.utils.EasyLog;
 
-public class CardDataBaseService{
+public class CardDataBaseService {
     private final String TAG = "CardDataBaseService";
     private AppDataBase mDataBase;
     private Context mContext;
+    private static final String TABLE_NAME = "LauncherCard";
+    private static final String DB_NAME = "card_db";
     private final Handler mHandler = new android.os.Handler(Looper.getMainLooper());
 
     public CardDataBaseService(Context context) {
@@ -28,9 +34,24 @@ public class CardDataBaseService{
     private void createDataBase(Context context) {
         if (mDataBase == null) {
             mDataBase = Room.databaseBuilder(context.getApplicationContext(),
-                    AppDataBase.class, "card_db").build();
+                            AppDataBase.class, DB_NAME)
+                    .addMigrations(MIGRATION_1_2)
+                    .build();
         }
     }
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("create table temp_table " +
+                    "( 'position' INTEGER NOT NULL, 'inHome' INTEGER NOT NULL, 'name' TEXT, 'type' INTEGER PRIMARY KEY NOT NULL, 'canExpand' INTEGER NOT NULL, " +
+                    "'mSelectBgResName' TEXT, 'mUnselectBgResName' TEXT)");
+            database.execSQL("insert into temp_table (position, inHome, name ,type ,canExpand ,mSelectBgResName, mUnselectBgResName)" +
+                    " select position, inHome, name ,type ,canExpand ,mSelectBgResName, mUnselectBgResName from " + TABLE_NAME);
+            database.execSQL("drop table " + TABLE_NAME);
+            database.execSQL("alter table temp_table rename to " + TABLE_NAME);
+        }
+    };
 
     private List<LauncherCard> getAllCardsSync() {
         if (mDataBase == null) {
@@ -60,6 +81,7 @@ public class CardDataBaseService{
             }
         });
     }
+
     public void saveCards(List<LauncherCard> launcherCards) {
         if (mDataBase == null) {
             createDataBase(mContext);
@@ -67,7 +89,7 @@ public class CardDataBaseService{
         AsyncSchedule.execute(new Runnable() {
             @Override
             public void run() {
-                EasyLog.d(TAG, "saveCards :"+launcherCards);
+                EasyLog.d(TAG, "saveCards :" + launcherCards);
                 if (launcherCards == null || launcherCards.isEmpty()) {
                     return;
                 }
@@ -86,7 +108,7 @@ public class CardDataBaseService{
         AsyncSchedule.execute(new Runnable() {
             @Override
             public void run() {
-                EasyLog.d(TAG, "updateCards :"+launcherCards);
+                EasyLog.d(TAG, "updateCards :" + launcherCards);
                 if (launcherCards == null || launcherCards.isEmpty()) {
                     return;
                 }
